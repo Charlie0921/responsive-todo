81c730f543638208bcce27b6afcee5441ae5d538
Kunjoong(Charlie) Kim <90428123+Charlie0921@users.noreply.github.com>
2025-02-21 15:36:52 +0000

    installed Django setup

---
+++ ResponsiveTodo/TodoApp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+from . models import Task
+
+def index(request):
+
+    tasks = Task.objects.all()
+
+    context = {'tasks': tasks}
+
+    return render(request, 'index.html', context)
+
